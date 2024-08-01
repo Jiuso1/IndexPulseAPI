5a844d6947c6b06220adf2ac2334b29d26b3ef37
Jiuso <jesusgg200x@gmail.com>
2024-08-01 15:44:23 +0200

    File uploading working!

---
+++ src/main/java/com/team/indexpulseapi/controller/IndexApiController.java
@@ -0,0 +1,10 @@
+package com.team.indexpulseapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/index_api")
+public class IndexApiController {
+
+}
