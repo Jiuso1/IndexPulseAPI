0903dd4abde8257a0cbdc5fb1fdc2af000e976ec
Jiuso <jesusgg200x@gmail.com>
2024-08-11 15:00:36 +0200

    Red letters (error output) working!

--- src/main/java/com/team/indexpulseapi/controller/IndexApiController.java
+++ src/main/java/com/team/indexpulseapi/controller/IndexApiController.java
@@ -4,20 +4,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @RestController
 @RequestMapping("/index_api")
 public class IndexApiController {
+
+    final boolean isWindows;
+
+    IndexApiController() {
+        isWindows = System.getProperty("os.name").toLowerCase().contains("windows");
+    }
+
     @GetMapping("/test")
     public void getTest() {
         String output = null;
-        try {
-            output = execCmd("cmd.exe /c C:/Users/jesus/AppData/Roaming/npm/gis.cmd google.com " +
-                    "--path C:/Users/jesus/Documents/Java/upwork/google-indexing-script/service_account.json");
-            System.out.println(output);
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+        if (!isWindows) {
+            //AWS Server Linux code.
+        } else {
+            try {
+                output = execCmdJiuso("cmd.exe /c C:/Users/jesus/AppData/Roaming/npm/gis.cmd google.com " +
+                        "--path C:/Users/jesus/Documents/Java/upwork/google-indexing-script/service_account.json");
+                System.out.println(output);
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+
         }
     }
 
@@ -25,5 +40,26 @@ public class IndexApiController {
     public static String execCmd(String cmd) throws java.io.IOException {
         java.util.Scanner s = new java.util.Scanner(Runtime.getRuntime().exec(cmd).getInputStream()).useDelimiter("\\A");
         return s.hasNext() ? s.next() : "";
+    }
+
+    public static String execCmdJiuso(String cmd) throws java.io.IOException {
+        String error = "";
+        String normal = "";
+        String output = "";
+        java.util.Scanner errorScanner = new java.util.Scanner(Runtime.getRuntime().exec(cmd).getErrorStream());
+        java.util.Scanner normalScanner = new java.util.Scanner(Runtime.getRuntime().exec(cmd).getInputStream());
+
+        while (errorScanner.hasNext()) {
+            error += errorScanner.next() + "\n";
+        }
+
+        while (normalScanner.hasNext()) {
+            normal += normalScanner.next() + "\n";
+        }
+
+        output += error;
+        output += normal;
+
+        return output;
     }
 }
